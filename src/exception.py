53e9fbb8ebdfeaaa00c6b451f5559c5ab4381757
sandeepbandi924 <bandisandeep1374@gmail.com>
2024-12-10 14:45:55 +0530

    EDA and FE

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exe_tb = error_detail.exc_info()
@@ -25,5 +25,5 @@ class CustomException(Exception):
 #     try:
 #         a=1/0
 #     except Exception as e:
-#         logging.info('Division by Zero Error')
+#         logging.info('Division by Zero')
 #         raise CustomException(e,sys)
